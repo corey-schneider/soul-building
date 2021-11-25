7a0ba2a190b045cf817c846d745bab3ad8c3be6a
Corey <cschne11@uncc.edu>
2021-11-24 21:19:55 -0500

    added support for main_photo; populated Portfolio page

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+    has_one_attached :main_photo
+    has_many_attached :photos
     validates :title, presence: true, length: { minimum: 5 }
     validates :body, presence: true, length: { minimum: 10 }
 end
