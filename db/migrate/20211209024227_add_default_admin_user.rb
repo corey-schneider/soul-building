863a9cf4047ac74251ad27605250efba97b8a519
Corey Schneider <coreyschneider2@gmail.com>
2021-12-12 16:39:08 -0500

    removed unnecessary line

--- db/migrate/20211209024227_add_default_admin_user.rb
+++ db/migrate/20211209024227_add_default_admin_user.rb
@@ -1,6 +1,5 @@
 class AddDefaultAdminUser < ActiveRecord::Migration[6.1]
   def change
-    # user = User.new({email: 'soulbuilding@mail.com', password: 'thisIsAFakePassword1', password_confirmation: 'thisIsAFakePassword1'})
     User.create(email: 'soulbuilding@mail.com', password: 'thisIsAFakePassword1', first_name: "James", last_name: "Schneider")
   end
 end
