115e028c6778419f463ba5a27f62463c16f8a155
Corey Schneider <coreyschneider2@gmail.com>
2021-12-12 20:36:30 -0500

    changed routes to use /about instead of /about/index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'contact_us/index'
-  get 'portfolio/index'
-  get 'about/index'
-  get 'home/index'
+  get 'contact_us', to: 'contact_us#index'
+  get 'portfolio', to: 'portfolio#index'
+  get 'about', to: 'about#index'
+  get 'home', to: 'home#index'
   root "home#index"
 
   resources :posts
