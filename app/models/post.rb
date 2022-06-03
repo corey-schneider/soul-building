d89c2c0f0d38164d073c4ba24bc085d8d8d93504
Corey Schneider <coreyschneider2@gmail.com>
2022-06-02 22:04:07 -0400

    add trix editor (wysiwyg)

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
   belongs_to :user
   validates :title, presence: true, length: { minimum: 5 }
   validates :body, presence: true, length: { minimum: 10 }
+  has_rich_text :body
 
   def self.most_recent(num)
     order(updated_at: :desc).first(num)
