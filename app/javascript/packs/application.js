c6db46750a894e5d7e6b7b8341752a033b6de165
Corey Schneider <coreyschneider2@gmail.com>
2022-06-29 22:41:36 -0400

    replace turbolinks with turbo; add jquery and lightbox2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,16 +4,23 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import 'bootstrap'
 import '../stylesheets/application'
+require('@hotwired/turbo-rails')
+require('jquery')
 // require("stylesheets/application.scss")
-
 Rails.start()
-Turbolinks.start()
 ActiveStorage.start()
 
 require("trix")
 require("@rails/actiontext")
+document.addEventListener('turbo:load', function () {
+  require('lightbox2').option({
+    'fadeDuration': 100,
+    'wrapAround': true,
+    'alwaysShowNavOnTouchDevices': true
+  })
+})
