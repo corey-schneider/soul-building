1b4a74c38c748d02c5d2ed485e1fa8e5aedb6c56
Corey Schneider <coreyschneider2@gmail.com>
2022-11-13 22:50:39 -0500

    add custom fonts

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -11,6 +11,8 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        heading: ['Plus Jakarta Sans'],
+        opensans: ['Open Sans']
       },
     },
   },
