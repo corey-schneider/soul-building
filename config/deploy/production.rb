9881c53c840220a9505d855bc7b5331d062bf940
Corey Schneider <coreyschneider2@gmail.com>
2025-09-09 17:21:01 -0400

    update user to deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server '167.172.141.55', user: 'root', roles: %w[app db web]
+server '167.172.141.55', user: 'deploy', roles: %w[app db web]
 
 # role-based syntax
 # ==================
