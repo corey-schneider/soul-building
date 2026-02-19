db1d2d860f66402b7ce4d081b93a7de1b748de6b
Corey Schneider <coreyschneider2@gmail.com>
2026-02-18 23:13:13 -0500

    add validations for tickets (email, phone, body length)

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,7 @@
 class Ticket < ApplicationRecord
   enum status: %i[open closed]
-  validates_presence_of :first_name, :last_name, :email, :phone, :body
+  validates :first_name, :last_name, :email, :phone, :body, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :phone, format: { with: /\A\+?[0-9\s().-]{7,}\z/ }
+  validates_length_of :body, minimum: 10, maximum: 800
 end
