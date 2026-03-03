ee89c70780efd0a4f27fcc25b61cca529ccf8cb1
Corey Schneider <coreyschneider2@gmail.com>
2026-03-02 22:00:23 -0500

    (untested in prod) sonnet 4.6 code

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -2,10 +2,6 @@
 
 module NavigationHelper
   def nav_link(text, path, **options)
-    if path == contact_us_path
-      options[:data] = { turbo: false }
-    end
-    
     content_tag(:div, class: 'nav-item px-2 ' + ('active' if current_page?(path)).to_s) do
       link_to text, path, class: 'nav-link', **options
     end
